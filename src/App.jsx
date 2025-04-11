60e3e2a378aedd921c001c2c07a998a4d5555a55
Vishwa Sandun Chandrasekara <sandunv05@gmail.com>
2025-04-11 22:16:04 +0530

    3rd commit
    2025/04/11

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,122 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { supabase } from './createClient'
 
 const App = () => {
+  const [users, setUsers] = useState([])
 
-const[users,setUsers] = useState([])
-
-const [user,setUser] = useState({
-  name:'',
-  age:0
-})
-
-// console.log(user);
-
-
-
-useEffect(() => {
-  fetchUsers()
-},[])
-
-async function fetchUsers(){
-  const {data} = await supabase
-  .from('users')
-  .select('*')
-  setUsers(data)
-  // console.log(data);
-}
-
-function handlechange(e){
-  const {name,value} = e.target
-  setUser({
-    ...user,
-    [name]:value
+  const [user, setUser] = useState({
+    name: '',
+    age: 0
   })
-}
 
-async function createUser(){
-  await supabase
-  .from('users')
-  .insert(
-    { name: user.name, age: user.age }
-  )
-}
+  const [user2, setUser2] = useState({
+    id: null,
+    name: '',
+    age: 0
+  })
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
+  async function fetchUsers() {
+    const { data } = await supabase
+      .from('users')
+      .select('*')
+    setUsers(data)
+  }
+
+  function handlechange(e) {
+    const { name, value } = e.target
+    setUser({
+      ...user,
+      [name]: value
+    })
+  }
+
+  function handlechange2(e) {
+    const { name, value } = e.target
+    setUser2({
+      ...user2,
+      [name]: value
+    })
+  }
+
+  async function createUser() {
+    
+    await supabase
+      .from('users')
+      .insert({ name: user.name, age: user.age })
+    fetchUsers()
+    
+  }
+
+  async function deleteUser(id) {
+    await supabase
+      .from('users')
+      .delete()
+      .eq('id', id)
+
+    fetchUsers()
+  }
+
+  async function updateUser() {
+    
+    await supabase
+      .from('users')
+      .update({ name: user2.name, age: user2.age })
+      .eq('id', user2.id)
+
+    fetchUsers()
+   
+  }
+
+  function displayUser(id) {
+    const selectedUser = users.find((user) => user.id === id)
+    if (selectedUser) {
+      setUser2({
+        id: selectedUser.id,
+        name: selectedUser.name,
+        age: selectedUser.age
+      })
+    }
+  }
 
   return (
     <div>
-
-      <form action="">
-
-        <input type="text" placeholder='Name' name='name' onChange={handlechange}/>
-        <input type="number" placeholder='Age' name='age' onChange={handlechange}/>
-        <button type='submit' onClick={createUser}>Submit</button>
-
-
-
+      <form onSubmit={createUser}>
+        <input
+          type="text"
+          placeholder='Name'
+          name='name'
+          value={user.name}
+          onChange={handlechange}
+        />
+        <input
+          type="number"
+          placeholder='Age'
+          name='age'
+          value={user.age}
+          onChange={handlechange}
+        />
+        <button type='submit'>Submit</button>
       </form>
 
-
+      <form onSubmit={updateUser}>
+        <input
+          type="text"
+          name='name'
+          onChange={handlechange2}
+          value={user2.name}
+        />
+        <input
+          type="number"
+          name='age'
+          onChange={handlechange2}
+          value={user2.age}
+        />
+        <button type='submit'>Save Changes</button>
+      </form>
 
       <table>
         <thead>
@@ -63,18 +124,22 @@ async function createUser(){
             <th>Id</th>
             <th>Name</th>
             <th>Age</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
           {users.map((user) => (
-            <tr>
+            <tr key={user.id}>
               <td>{user.id}</td>
               <td>{user.name}</td>
               <td>{user.age}</td>
+              <td>
+                <button onClick={() => deleteUser(user.id)}>Delete</button>
+                <button onClick={() => displayUser(user.id)}>Edit</button>
+              </td>
             </tr>
           ))}
         </tbody>
-       
       </table>
     </div>
   )
